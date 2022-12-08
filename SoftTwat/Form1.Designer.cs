4b7350d976323fa4989e9cfdf6992ab1c8099a8d
Jack Banasiak <47184534+Hackerax1@users.noreply.github.com>
2022-12-08 13:23:17 -0500

    Add files via upload

---
+++ SoftTwat/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace SoftTwat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listButton = new System.Windows.Forms.Button();
+            this.folderTextBox1 = new System.Windows.Forms.TextBox();
+            this.compareButton = new System.Windows.Forms.Button();
+            this.folderTextBox2 = new System.Windows.Forms.TextBox();
+            this.folderTextBox3 = new System.Windows.Forms.TextBox();
+            this.combineButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listButton
+            // 
+            this.listButton.Location = new System.Drawing.Point(572, 324);
+            this.listButton.Name = "listButton";
+            this.listButton.Size = new System.Drawing.Size(117, 71);
+            this.listButton.TabIndex = 0;
+            this.listButton.Text = "Get Folders";
+            this.listButton.UseVisualStyleBackColor = true;
+            this.listButton.Click += new System.EventHandler(this.listButton_Click_1);
+            // 
+            // folderTextBox1
+            // 
+            this.folderTextBox1.Location = new System.Drawing.Point(141, 52);
+            this.folderTextBox1.Name = "folderTextBox1";
+            this.folderTextBox1.Size = new System.Drawing.Size(461, 22);
+            this.folderTextBox1.TabIndex = 1;
+            // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(67, 324);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(129, 71);
+            this.compareButton.TabIndex = 2;
+            this.compareButton.Text = "Compare Libraries";
+            this.compareButton.UseVisualStyleBackColor = true;
+            this.compareButton.Click += new System.EventHandler(this.compareButton_Click);
+            // 
+            // folderTextBox2
+            // 
+            this.folderTextBox2.Location = new System.Drawing.Point(141, 95);
+            this.folderTextBox2.Name = "folderTextBox2";
+            this.folderTextBox2.Size = new System.Drawing.Size(461, 22);
+            this.folderTextBox2.TabIndex = 3;
+            // 
+            // folderTextBox3
+            // 
+            this.folderTextBox3.Location = new System.Drawing.Point(141, 142);
+            this.folderTextBox3.Name = "folderTextBox3";
+            this.folderTextBox3.Size = new System.Drawing.Size(461, 22);
+            this.folderTextBox3.TabIndex = 4;
+            // 
+            // combineButton
+            // 
+            this.combineButton.Location = new System.Drawing.Point(308, 324);
+            this.combineButton.Name = "combineButton";
+            this.combineButton.Size = new System.Drawing.Size(128, 71);
+            this.combineButton.TabIndex = 5;
+            this.combineButton.Text = "Combine Library Lists";
+            this.combineButton.UseVisualStyleBackColor = true;
+            this.combineButton.Click += new System.EventHandler(this.combineButton_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input up to three directories of game folders";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combineButton);
+            this.Controls.Add(this.folderTextBox3);
+            this.Controls.Add(this.folderTextBox2);
+            this.Controls.Add(this.compareButton);
+            this.Controls.Add(this.folderTextBox1);
+            this.Controls.Add(this.listButton);
+            this.Name = "MainForm";
+            this.Text = "Soft Twat";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listButton;
+        private System.Windows.Forms.TextBox folderTextBox1;
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.TextBox folderTextBox2;
+        private System.Windows.Forms.TextBox folderTextBox3;
+        private System.Windows.Forms.Button combineButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SoftTwat/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SoftTwat
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Set the initial value of the folder text box
+            folderTextBox1.Text = @"C:\";
+        }
+        private void listButton_Click_1(object sender, EventArgs e)
+        {
+            // Get the directories specified by the user
+            List<string> directories = new List<string>();
+            if (!string.IsNullOrEmpty(folderTextBox1.Text))
+            {
+                directories.Add(folderTextBox1.Text);
+            }
+            if (!string.IsNullOrEmpty(folderTextBox2.Text))
+            {
+                directories.Add(folderTextBox2.Text);
+            }
+            if (!string.IsNullOrEmpty(folderTextBox3.Text))
+            {
+                directories.Add(folderTextBox3.Text);
+            }
+
+            // Get a list of the names of all the folders in the directories
+            List<string> folders = new List<string>();
+            foreach (string directory in directories)
+            {
+                folders.AddRange(Directory.GetDirectories(directory));
+            }
+            // Alphabetize the list of folder names
+            folders.Sort();
+
+            // Show the "Save File" dialog
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveDialog.FilterIndex = 1;
+                saveDialog.RestoreDirectory = true;
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the folder path from the text box
+                    // string folder = folderTextBox1.Text;
+
+                    // Get a list of all the folders in the specified folder
+                    //string[] folderNames = Directory.GetDirectories(folder);
+
+                    // Write the list of folder names to a text file
+                    using (StreamWriter writer = new StreamWriter(saveDialog.FileName))
+                    {
+                        foreach (string folderName in folders)
+                        {
+                            writer.WriteLine(Path.GetFileName(folderName));
+                        }
+                    }
+
+                    MessageBox.Show("Folder names written to " + saveDialog.FileName);
+                }
+            }
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+            if (compareButton.Enabled == false)
+            {
+                // Do not execute the method again
+                return;
+            }
+
+            // Disable the "Compare" button to prevent multiple executions
+            compareButton.Enabled = false;
+
+            // Show the "Open File" dialog
+            using (OpenFileDialog openDialog = new OpenFileDialog())
+            {
+                openDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openDialog.FilterIndex = 1;
+                openDialog.Multiselect = true;
+                openDialog.RestoreDirectory = true;
+
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Read the contents of the selected files
+                    List<string[]> linesList = new List<string[]>();
+                    foreach (string fileName in openDialog.FileNames)
+                    {
+                        linesList.Add(File.ReadAllLines(fileName));
+                    }
+                    List<string> matchingLines = new List<string>();
+                    HashSet<string> matchingLinesSet = new HashSet<string>();
+                    bool match = true;
+                    foreach (string[] lines in linesList)
+                    {
+                        foreach (string line in lines)
+                        {
+                            match = true;
+                            foreach (string[] otherLines in linesList)
+                            {
+                                if (!Array.Exists(otherLines, element => element == line))
+                                {
+                                    match = false;
+                                    break;
+                                }
+                            }
+
+                            if (match && !matchingLinesSet.Contains(line))
+                            {
+                                matchingLines.Add(line);
+                                matchingLinesSet.Add(line);
+                            }
+                        }
+
+                        // Show the "Save File" dialog
+                        using (SaveFileDialog saveDialog = new SaveFileDialog())
+                        {
+                            saveDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                            saveDialog.FilterIndex = 1;
+                            saveDialog.RestoreDirectory = true;
+
+                            if (saveDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                // Write the list of matching lines to the selected file
+                                using (StreamWriter writer = new StreamWriter(saveDialog.FileName))
+                                {
+                                    foreach (string matchingLine in matchingLines)
+                                    {
+                                        writer.WriteLine(matchingLine);
+                                    }
+                                }
+
+                                MessageBox.Show("Matching lines written to " + saveDialog.FileName);
+
+                            }
+
+                        }
+                    }
+                    compareButton.Enabled = true;
+                }
+            }
+        }
+        private void combineButton_Click_1(object sender, EventArgs e)
+        {
+            // Show the "Open File" dialog
+            using (OpenFileDialog openDialog = new OpenFileDialog())
+            {
+                openDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openDialog.FilterIndex = 1;
+                openDialog.Multiselect = true;
+                openDialog.RestoreDirectory = true;
+
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Read the contents of the selected files
+                    List<string> lines = new List<string>();
+                    foreach (string fileName in openDialog.FileNames)
+                    {
+                        lines.AddRange(File.ReadAllLines(fileName));
+                    }
+                    // Remove duplicate lines
+                    HashSet<string> linesSet = new HashSet<string>(lines);
+                    lines = new List<string>(linesSet);
+                    // Sort the lines alphabetically
+                    lines.Sort();
+
+                    // Show the "Save File" dialog
+                    using (SaveFileDialog saveDialog = new SaveFileDialog())
+                    {
+                        saveDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                        saveDialog.FilterIndex = 1;
+                        saveDialog.RestoreDirectory = true;
+
+                        if (saveDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            // Write the combined and alphabetized lines to the selected file
+                            using (StreamWriter writer = new StreamWriter(saveDialog.FileName))
+                            {
+                                foreach (string line in lines)
+                                {
+                                    writer.WriteLine(line);
+                                }
+                            }
+
+                            MessageBox.Show("Combined and alphabetized lines written to " + saveDialog.FileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
